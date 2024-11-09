accc3f6e4c0d8d768fa32d5b559ee15a8feb45a5
BrunoSX <brunodeveloper85@gmail.com>
2024-11-09 14:55:06 -0300

    Remove armhf paths and change compression level for zstd

--- pulseaudio-module/create-asset.sh
+++ pulseaudio-module/create-asset.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-mkdir -p pulseaudio/modules/arm64
-mkdir -p pulseaudio/modules/armhf
+mkdir -p pulseaudio/modules
 
-cp -a ../output/aarch64-linux-gnu/modules/libprotocol-native.so pulseaudio/modules/arm64
-cp -a ../output/aarch64-linux-gnu/modules/module-native-protocol-unix.so pulseaudio/modules/arm64
+cp -a ../output/aarch64-linux-gnu/modules/libprotocol-native.so pulseaudio/modules
+cp -a ../output/aarch64-linux-gnu/modules/module-native-protocol-unix.so pulseaudio/modules
 
-cp -a ../output/arm-linux-gnueabihf/modules/libprotocol-native.so pulseaudio/modules/armhf
-cp -a ../output/arm-linux-gnueabihf/modules/module-native-protocol-unix.so pulseaudio/modules/armhf
+cp -a build64/module-aaudio-sink.so pulseaudio/modules
 
-cp -a build64/module-aaudio-sink.so pulseaudio/modules/arm64
-cp -a build/module-aaudio-sink.so pulseaudio/modules/armhf
-
-tar -I 'zstd -19' -cf pulseaudio.tzst -C pulseaudio .
+tar -I 'zstd --ultra -22' -cf pulseaudio.tzst -C pulseaudio .
 
 rm -r pulseaudio
